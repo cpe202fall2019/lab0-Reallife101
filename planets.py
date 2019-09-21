93988a65740a56da05c3acd1b8cb398b6cb03c29
Reallife101 <ryan.ngoon@gmail.com>
2019-09-21 13:21:37 -0700

    I added the code to display the weight of a person on different planets

--- planets.py
+++ planets.py
@@ -1,7 +1,13 @@
+import sys
+
+from pip._vendor.distlib.compat import raw_input
+
+
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    # write your code here
+    weight = float(input("What do you weigh on earth? "))
+    print("\nOn Mars you would weigh " + str(weight * 0.38) + " pounds.\nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds. ")
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
