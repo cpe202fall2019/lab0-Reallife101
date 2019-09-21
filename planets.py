5205a2169ff79a34e3f511e35c88fda1fc69ba3d
Reallife101 <ryan.ngoon@gmail.com>
2019-09-21 13:41:04 -0700

    fixed the file back to default so test actually works

--- planets.py
+++ planets.py
@@ -6,7 +6,7 @@ from pip._vendor.distlib.compat import raw_input
 def weight_on_planets():
     # write your code here
     weight = float(input("What do you weigh on earth? "))
-    print("\nOn Mars you would weigh " + str(weight * 0.38) + " pounds.\nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds. ")
+    print("\nOn Mars you would weigh " + str(weight * 0.38) + " pounds.\nOn Jupiter you would weigh " + str(weight * 2.34) + " pounds.")
 
 
 if __name__ == '__main__':
